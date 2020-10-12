3a6e58daeeb1ecdb52caaec306a905154cb65574
Promise Fru <promisefru1@gmail.com>
2020-10-12 21:55:49 +0100

    why does split not work ??

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('working');
+app.get('/api/convert', (req, res) => {
+    var input = req.params.input;
+    var myArr = input.split(/(\d+)/);
+    var num = myArr[0];
+    var unit = myArr[1];
+
+    res.json({
+        num: num,
+        unit: unit
+    })
 })
 
 app.listen(3000, console.log('Server running ...'));
