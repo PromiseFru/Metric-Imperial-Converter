4643617d6ea5ef21a5a041bfb546ea8078db3082
Promise Fru <promisefru1@gmail.com>
2020-10-13 18:25:29 +0100

    regex float added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/api/convert', (req, res) => {
     var input = req.query.input;
-    myArr = input.split(/(\d+)/);
+    myArr = input.split(/(\d+\.\d+|\d+)/);
     var num = myArr[1];
     var unit = myArr[2];
 
