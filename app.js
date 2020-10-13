3ae665d13c46a61a30101826448f05a24597c5d8
Promise Fru <promisefru1@gmail.com>
2020-10-13 17:53:32 +0100

    code made simple

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/api/convert', (req, res) => {
-    function stringSplit(str){
-        var myArr = [];
-        var split = str.split(/(\d+)/);
-        split.forEach(ele => {
-            myArr.push(ele)
-        })
-        return myArr
-    }
     var input = req.query.input;
-    testArr = stringSplit(input);
-    var num = testArr[1];
-    var unit = testArr[2];
+    myArr = input.split(/(\d+)/);
+    var num = myArr[1];
+    var unit = myArr[0];
 
     res.json({
         num: num,
