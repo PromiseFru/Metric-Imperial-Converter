2dac105d0857b8dcf11025aa37bdf7d541d52bda
PromiseFru <promisefru1@gmail.com>
2020-10-15 13:47:45 +0100

    units logic created

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ app.get('/api/convert', (req, res) => {
     myArr = input.split(/(\d+\.\d+|\d+)/);
     var num = function(){
         if(typeof Number(myArr[1]) == 'number' && !isNaN(Number(myArr[1])) && isFinite(Number(myArr[1]))){
-            return myArr[1]
+            return myArr[1];
         }else{
             return "invalid number";
         }
     }
-    var unit = myArr[2];
+    var unit = function(){
+        if(typeof myArr[2] == 'string' || myArr[2] instanceof String){
+            return myArr[2];
+        }else{
+            return "invalid unit";
+        }
+    }
 
     res.json({
         num: num(),
-        unit: unit
+        unit: unit()
     })
 })
 
