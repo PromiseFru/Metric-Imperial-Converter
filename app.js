1de5829dc80d212672d9ab16ca4f80072744125c
Promise Fru <promisefru1@gmail.com>
2020-10-16 06:28:37 +0100

    using js eval() function

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.get('/api/convert', (req, res) => {
     var input = req.query.input;
-    //myArr = input.split(/(\d+\.\d+|\d+)/);
+
     firstCharIndex = input.match('[a-zA-Z]').index;
-    var num = Number(input.slice(0, firstCharIndex));
+    var num = eval(input.slice(0, firstCharIndex));
     var unit = input.slice(firstCharIndex);
+
+    // 
 
     res.json({
         num: num,
